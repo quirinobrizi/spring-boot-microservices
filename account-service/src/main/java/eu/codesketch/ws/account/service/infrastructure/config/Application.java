c031422c4be8aaca610549f8e98ab4e8a3e4fe82
Quirino Brizi <quirino.brizi@gmail.com>
2016-04-17 19:18:27 +0100

    introduce docker compose, integrate with spring-cloud-extension to be able to use docker container ip address on request

--- account-service/src/main/java/eu/codesketch/ws/account/service/infrastructure/config/Application.java
+++ account-service/src/main/java/eu/codesketch/ws/account/service/infrastructure/config/Application.java
@@ -3,11 +3,17 @@
  */
 package eu.codesketch.ws.account.service.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import eu.codesketch.spring.cloud.ribbon.IpAddressLoadBalancerClient;
 
 /**
  * @author quirino
@@ -20,7 +26,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("eu.codesketch.ws.account.service")
 public class Application {
 
+    @Autowired
+    private SpringClientFactory clientFactory;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public LoadBalancerClient getDockerLoadBalancerClient() {
+        return new IpAddressLoadBalancerClient(clientFactory);
     }
 }
--- brew-service/src/main/java/eu/codesketch/ws/brew/service/infrastructure/config/Application.java
+++ brew-service/src/main/java/eu/codesketch/ws/brew/service/infrastructure/config/Application.java
@@ -3,13 +3,18 @@
  */
 package eu.codesketch.ws.brew.service.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import eu.codesketch.spring.cloud.ribbon.IpAddressLoadBalancerClient;
 
 /**
  * @author quirino
@@ -18,19 +23,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableDiscoveryClient
 @EnableAutoConfiguration
-@ComponentScan("eu.codesketch.ws.brew.service")
+@ComponentScan({ "eu.codesketch.ws.brew.service" })
 public class Application {
 
     @Value("${db.username}")
     private String username;
+
+    @Autowired
+    private SpringClientFactory clientFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public String dbUsername() {
-        System.err.println(">>>>>>>>>>>>>>> " + username);
-        return username;
+    public LoadBalancerClient getIpAddressLoadBalancerClient() {
+        return new IpAddressLoadBalancerClient(clientFactory);
     }
 }
